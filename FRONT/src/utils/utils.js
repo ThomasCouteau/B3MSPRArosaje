34c5ffcb3432ab994ee654468df6a573ffefec1d
ThomasCouteau <mrthomas.couteau@gmail.com>
2023-02-24 17:08:24 +0100

    changement url api

--- FRONT/src/utils/utils.js
+++ FRONT/src/utils/utils.js
@@ -1,2 +1,2 @@
-//export const API_URL = "https://arosaje-api.ibsolutions.cloud";
-export const API_URL = "http://localhost:1418";
+export const API_URL = "https://arosaje-api.ibsolutions.cloud";
+//export const API_URL = "http://localhost:1418";

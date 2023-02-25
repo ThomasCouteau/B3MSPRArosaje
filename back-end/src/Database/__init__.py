8ad890e54d8675a2647e2bd682b9e5a47501f04d
Professeur Issou <alix.hamidou@gmail.com>
2023-02-25 20:03:49 +0100

    Self hosted db

--- back-end/src/Database/__init__.py
+++ back-end/src/Database/__init__.py
@@ -10,10 +10,12 @@
 """
 from supabase import create_client, Client
 
-
-API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV6eXFma3ZleWVpbmdrb3h5Z2htIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY3NzA4NzA4NSwiZXhwIjoxOTkyNjYzMDg1fQ.7Vs0ng29VYKE0ttjW1t3SXL4hS_lJbd63gCJsoUrxKg"
-URL = "https://ezyqfkveyeingkoxyghm.supabase.co"
-
+# En ligne
+# API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV6eXFma3ZleWVpbmdrb3h5Z2htIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY3NzA4NzA4NSwiZXhwIjoxOTkyNjYzMDg1fQ.7Vs0ng29VYKE0ttjW1t3SXL4hS_lJbd63gCJsoUrxKg"
+# URL = "https://ezyqfkveyeingkoxyghm.supabase.co"
+# Self-hosted
+API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJzZXJ2aWNlX3JvbGUiLAogICAgImlzcyI6ICJzdXBhYmFzZS1kZW1vIiwKICAgICJpYXQiOiAxNjQxNzY5MjAwLAogICAgImV4cCI6IDE3OTk1MzU2MDAKfQ.DaYlNEoUrrEn2Ig7tqibS-PHK5vgusbcbo7X36XVt4Q"
+URL = "http://192.168.1.173:8000"
 
 
 db: Client = create_client(URL, API_KEY)

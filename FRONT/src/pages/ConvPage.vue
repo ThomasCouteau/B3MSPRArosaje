0c46dc10af67d4e0ea6959822b2a19cb15635449
ThomasCouteau <mrthomas.couteau@gmail.com>
2023-03-16 13:58:26 +0100

    popup attention data mp

--- FRONT/src/pages/ConvPage.vue
+++ FRONT/src/pages/ConvPage.vue
@@ -1,17 +1,46 @@
 <template>
-  <q-page class="">
+  <q-page>
     <ChatMessage />
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from "vue";
+import { defineComponent, onMounted } from "vue";
 import ChatMessage from "src/components/ChatMessage.vue";
+import { useQuasar } from "quasar";
+import { Dialog } from "quasar";
 
 export default defineComponent({
   name: "ConvPage",
   components: {
     ChatMessage,
+  },
+  setup() {
+    const $q = useQuasar();
+    const privateMessages = localStorage.getItem("privateMessages");
+
+    const confirmMsg = () => {
+      if (privateMessages) {
+        return;
+      }
+      if (!privateMessages) {
+        $q.dialog({
+          title: "Informations personnelles",
+          message: "Veuillez ne partager que les informations nécessaires.",
+          cancel: false,
+          persistent: true,
+        })
+          .onOk(() => {
+            let msg = localStorage.setItem("privateMessages", true);
+          })
+          .onOk(() => {})
+          .onDismiss(() => {});
+      }
+    };
+
+    onMounted(() => {
+      confirmMsg();
+    });
   },
 });
 </script>
